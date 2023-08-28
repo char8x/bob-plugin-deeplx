880be986b5894fba0722e604b275f6d92ffabed7
Charles Xu <6831013+char8x@users.noreply.github.com>
2023-08-28 18:45:58 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,7 +5,6 @@
  # @LastEditors: Vincent Young
  # @LastEditTime: 2023-04-10 03:31:01
  # @FilePath: /bob-plugin-deeplx/release.sh
- # @Telegram: https://t.me/missuo
  # 
  # Copyright © 2023 by Vincent, All Rights Reserved. 
 ### 
@@ -15,7 +14,7 @@ zip -r -j bob-plugin-deeplx-$version.bobplugin src/*
 sha256_deeplx=$(sha256sum bob-plugin-deeplx-$version.bobplugin | cut -d ' ' -f 1)
 echo $sha256_deeplx
 
-download_link="https://github.com/missuo/bob-plugin-deeplx/releases/download/v$version/bob-plugin-deeplx-$version.bobplugin"
+download_link="https://github.com/char8x/bob-plugin-deeplx/releases/download/v$version/bob-plugin-deeplx-$version.bobplugin"
 
 new_version="{\"version\": \"$version\", \"desc\": \"Fix some prompt errors.\", \"sha256\": \"$sha256_deeplx\", \"url\": \"$download_link\", \"minBobVersion\": \"0.5.0\"}"
 
